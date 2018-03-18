3b161e0c18613e87a0a2ca8128ca08bf820bd04f
Russell Hay <russell.hay@gmail.com>
2018-03-18 10:27:38 -0700

    Stream stopping point

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,49 @@ extern crate chrono;
 use juniper::FieldResult;
 use rocket::response::content;
 use rocket::State;
+use std::collections::HashMap;
 
-struct Context {}
+mod schema;
+
+struct Context {
+    pub branches: HashMap<String, schema::Branch>,
+}
 impl juniper::Context for Context { }
+impl Context {
+    fn new() -> Context {
+        Context {
+            branches: HashMap::new()
+        }
+    }
+}
 
 struct Query;
 graphql_object!(Query: Context |&self| {
     field apiVersion() -> &str {
         "0.1"
     }
+
+    interfaces: [&schema::Branch]
+
+    field branch(&executor, name: String) -> &schema::Branch {
+        let context = executor.context();
+        context.branches.get(&name).unwrap()
+    }
+
+    field branches(&executor) -> Vec<&schema::Branch> {
+        let context = executor.context();
+        context.branches.values().collect()
+    }
 });
 
 struct Mutation;
 graphql_object!(Mutation: Context |&self| {
-    field apiVersion() -> &str {
-        "No"
+    field setStatus(&executor, name: String, status: schema::BranchStatus) -> FieldResult<&schema::Branch> {
+        let mut context = executor.context();
+        let branch = context.branches.remove(&name).unwrap();
+        let updated_branch = branch.set_status(&status).unwrap();
+        context.branches.insert(name.clone(), updated_branch);
+        Ok(context.branches.get(&name).unwrap())
     }
 });
 
@@ -45,8 +73,13 @@ fn post_graphql_handler(
 }
 
 fn main() {
+    let mut context = Context::new();
+    let master = schema::Branch::new("master".to_owned());
+    let development = schema::Branch::new("development".to_owned());
+    context.branches.insert(master.name.clone(), master);
+    context.branches.insert(development.name.clone(), development);
     rocket::ignite()
-        .manage(Context {})
+        .manage(context)
         .manage(Schema::new(
             Query {},
             Mutation {}
--- src/schema.rs
+++ src/schema.rs
@@ -1,18 +1,39 @@
 use chrono::prelude::*;
 
 #[derive(GraphQLEnum)]
-enum BranchStatus {
+pub enum BranchStatus {
     Open,
-    ShiproomNotify(bug_num: String),
-    ShiproomApprovalRequired(bug_num: String),
-    ClosedForBreakage(bug_num: String),
-    RecoveringFromBreakage(bug_num: String),
+    ShiproomNotify,           // bug_num must be set
+    ShiproomApprovalRequired, // bug_num must be set
+    ClosedForBreakage,        // bug_num must be set
+    RecoveringFromBreakage,   // bug_num must be set
 }
 
 #[derive(GraphQLObject)]
 #[graphql(description="a branch that contains checkin-openness status")]
-struct Branch {
-    name: String,
+pub struct Branch {
+    pub name: String,
     status: BranchStatus,
     last_updated: DateTime<Utc>,
+    bug_num: Option<String>,
+}
+
+impl Branch {
+    pub fn new(name: String) -> Branch {
+        Branch {
+            name: name,
+            status: BranchStatus::Open,
+            last_updated: Utc::now(),
+            bug_num: None
+        }
+    }
+
+    pub fn set_status(self, status: &BranchStatus) -> Result<Branch, ()> {
+        // TODO: Enforce Bug Number
+        Ok(Branch {
+            status: *status,
+            last_updated: Utc::now(),
+            ..self
+        })
+    }
 }
